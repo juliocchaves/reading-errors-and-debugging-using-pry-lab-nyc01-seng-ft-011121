1d7d2fb132fb67aebf4a35f51ba233a3b348b4dc
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-28 04:17:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,13 +1,11 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def generate_star_date
-  binding.pry
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
@@ -20,9 +18,7 @@ def greet_crew(crew)
 end
 
 def engage
-  binding.pry
   date = generate_star_date
-  binding.pry
   state_log(date)
   greet_crew(crew)
 end
